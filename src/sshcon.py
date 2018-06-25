fc27278d369be02795d18387552f2bcc4984a543
tjushengyang <tjushengyang@hotmail.com>
2018-06-26 00:23:33 +0800

    dev

--- src/sshcon.py
+++ src/sshcon.py
@@ -7,6 +7,7 @@ Created on 2018年6月25日
 '''
 import paramiko
 import time
+import sys
 class SshCon:
     def __init__(self,ip,port,username,passwd):
         self.info = {"ip":ip,"port":port,"username":username,"passwd":passwd}
@@ -17,10 +18,14 @@ class SshCon:
         try:
             ssh.connect(hostname=self.info['ip'],port=self.info['port'],
                              username=self.info['username'],password=self.info['passwd'])
-            ssh.get_transport()
+#             ssh.get_transport()
             #self.info['ip'] 其实就是字典中{'ip': ip} 对应的value，也就是实际IP
-            ssh.get_pty()
+#             ssh.get_pty()
             self.shell = ssh.invoke_shell()   #登录到远程主机，开一个终端
+            buff = '' #定义空字符串
+            recv = str(self.shell.recv(65535),encoding='ascii')   #收到打印返回    可以尝试不加decoding返回结果
+            print(recv) 
+            sys.stdout.flush()
         except paramiko.AuthenticationException as e:  #登录失败报错
             raise e
     
@@ -31,12 +36,10 @@ class SshCon:
         buff = '' #定义空字符串
         time.sleep(1)  # 返回可能有延迟，等待1s，正常应该通过判断--- END字符串来判断返回是否完成
         recv = str(self.shell.recv(65535),encoding='ascii')   #收到打印返回    可以尝试不加decoding返回结果
-#         print( self.shell.recv(65535))   #收到打印返回    可以尝试不加decoding返回结果
-        buff += recv
-        output = buff
-        return output  #返回结果输出        
+        print(recv) 
+        sys.stdout.flush()
+        return len(recv)
 
 if __name__ == '__main__':    #定义程序入口
-    sshcon = SshCon(ip= "10.248.154.103", port = 22, username = "ppsicp", passwd = 'Psci@1234')  #定义初始值
-    printout = sshcon.execute(cmd= 'pwd\n')   #输入指令
-    print(printout)
+    sshcon = SshCon(ip= "192.16.3.49", port = 22, username = "root", passwd = 'root')  #定义初始值
+    outlen = sshcon.execute(cmd= 'pwd\n')   #输入指令
